2cc5e38477860549f494011979af19b8ff51ae1a
Gabriel Batista <svbgabriel@gmail.com>
2020-03-07 10:09:22 -0300

    Implementa a animação do inimigo

--- src/br/com/svbgabriel/jelda/entities/Enemy.java
+++ src/br/com/svbgabriel/jelda/entities/Enemy.java
@@ -10,10 +10,19 @@ import br.com.svbgabriel.jelda.world.World;
 
 public class Enemy extends Entity {
 
-	private double speed = 1;
+	private int frames = 0;
+	private int maxFrames = 20;
+	private int index = 0;
+	private int maxIndex = 1;
+	private double speed = 0.4;
+
+	private BufferedImage[] sprites;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
+		super(x, y, width, height, null);
+		sprites = new BufferedImage[2];
+		sprites[0] = Game.spritesheet.getSprite(112, 16, 16, 16);
+		sprites[1] = Game.spritesheet.getSprite(128, 16, 16, 16);
 	}
 
 	public void tick() {
@@ -32,10 +41,19 @@ public class Enemy extends Entity {
 				&& !isColliding(getX(), (int) (y - speed))) {
 			y -= speed;
 		}
+
+		frames++;
+		if (frames == maxFrames) {
+			frames = 0;
+			index++;
+			if (index > maxIndex) {
+				index = 0;
+			}
+		}
 	}
 
 	public void render(Graphics g) {
-		super.render(g);
+		g.drawImage(sprites[index], getX() - Camera.x, getY() - Camera.y, null);
 	}
 
 	public boolean isColliding(int xNext, int yNext) {
