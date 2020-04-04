c9741cfba6b97d7e8290f63cb4fb652cca05946f
Gabriel Batista <svbgabriel@gmail.com>
2020-04-04 10:18:09 -0300

    Implementa o uso de fontes externas

--- src/br/com/svbgabriel/jelda/main/Game.java
+++ src/br/com/svbgabriel/jelda/main/Game.java
@@ -4,6 +4,7 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
@@ -12,6 +13,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -53,6 +56,9 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 
 	public UI ui;
 
+	public InputStream stream;
+	public Font pixelFont;
+
 	public static String gameState = "MENU";
 	private boolean showMessageGameOver = true;
 	private int framesGameOver = 0;
@@ -79,6 +85,16 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 		player = new Player(0, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16));
 		entities.add(player);
 		world = new World("/level1.png");
+
+		try {
+			stream = ClassLoader.getSystemClassLoader().getResourceAsStream("alterebro-pixel-font.ttf");
+			pixelFont = Font.createFont(Font.TRUETYPE_FONT, stream);
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		menu = new Menu();
 	}
 
@@ -183,7 +199,7 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 		g.dispose();
 		g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
-		g.setFont(new Font("arial", Font.BOLD, 20));
+		g.setFont(pixelFont.deriveFont(36f));
 		g.setColor(Color.WHITE);
 		g.drawString("Bullets: " + player.ammo, 580, 20);
 		if (gameState.equals("GAME_OVER")) {
@@ -273,7 +289,7 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 
 		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			gameState = "MENU";
-			menu.pause = true;
+			Menu.pause = true;
 		}
 
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
