d6612f77cf9ae12d0d66c3d7c2a014205cedeee7
Gabriel Batista <svbgabriel@gmail.com>
2020-10-04 20:57:54 -0300

    Implementa o método para gerar o mapa randomicamente

--- src/br/com/svbgabriel/jelda/world/World.java
+++ src/br/com/svbgabriel/jelda/world/World.java
@@ -68,6 +68,52 @@ public class World {
 		}
 	}
 
+	public World() {
+		Game.player.setX(0);
+		Game.player.setY(0);
+		WIDTH = 100;
+		HEIGHT = 100;
+		tiles = new Tile[WIDTH * HEIGHT];
+
+		for (int xx = 0; xx < WIDTH; xx++) {
+			for (int yy = 0; yy < HEIGHT; yy++) {
+				tiles[xx + yy * WIDTH] = new WallTile(xx * 16, yy * 16, Tile.TILE_WALL);
+			}
+		}
+
+		int dir = 0;
+		int xx = 0, yy = 0;
+
+		for (int i = 0; i < 200; i++) {
+			tiles[xx + yy * WIDTH] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+			if (dir == 0) {
+				// direita
+				if (xx < WIDTH) {
+					xx++;
+				}
+			} else if (dir == 1) {
+				// esquerda
+				if (xx > 0) {
+					xx--;
+				}
+			} else if (dir == 2) {
+				// baixo
+				if (yy < HEIGHT) {
+					yy++;
+				}
+			} else if (dir == 3) {
+				// cima
+				if (yy > 0) {
+					yy--;
+				}
+			}
+
+			if (Game.rand.nextInt(100) < 30) {
+				dir = Game.rand.nextInt(4);
+			}
+		}
+	}
+
 	// Verifica se o tile para onde está se movendo esta livre
 	public static boolean isFree(int xNext, int yNext) {
 		int x1 = xNext / TILE_SIZE;
